c02f0b667d1de0d3e14d0bbc99d4c1390cdf7076
Jorge <daniel.--96@hotmail.com>
2023-09-15 12:31:30 -0500

    fix component to update canvas correctly

--- src/app/page/cpu/timelapse-multicore-usage/timelapse-single-usage/timelapse-single-usage.component.ts
+++ src/app/page/cpu/timelapse-multicore-usage/timelapse-single-usage/timelapse-single-usage.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { ChartConfiguration, ChartOptions } from 'chart.js';
 import { BaseChartDirective, NgChartsModule } from 'ng2-charts';
 import { Observable, Subscription } from 'rxjs';
@@ -17,66 +17,48 @@ export class TimelapseSingleUsageComponent {
   @Input() core_number: number = 0;
   @Input() core_data: number[] = [];
 
-  @ViewChild(BaseChartDirective) chart!: BaseChartDirective;
+  @ViewChild('core_chart') chart?: ElementRef<HTMLCanvasElement>;
 
-  public lineChartData: ChartConfiguration<'line'>['data'] = {
-    labels: [],
-    datasets: [
-      {
-        data: [],
-        label: 'Usage',
-        //fill: false,
-        //tension: 0.1,
-        borderWidth: 1,
-        pointRadius: 0,
-        //borderColor: 'black',
-        backgroundColor: 'limegreen'
-      },
-    ]
-  };
-  public lineChartOptions: ChartOptions<'line'> = {
-    //parsing: false,
-    responsive: false,
-    animation: false,
-    //spanGaps: true,
-    scales: {
-      x: { display: false, min:0, max: 120},
-      y: { display: true, min:0, max: 100, beginAtZero: false }
-    },
-    plugins: { legend: { display: false } }
-  };
-  public lineChartLegend = false;
+  chart_context!: CanvasRenderingContext2D;
 
   constructor(
     private signalService: CpuDataUpdateServiceService
   ) { }
 
   ngOnInit() {
-    this.eventsSubscription = this.signalService.message$.subscribe(() => {
-      this.on_core_data_updated();
+
+  }
+
+  ngAfterViewInit(){
+    if(!this.chart) return;
+    this.chart_context = this.chart?.nativeElement.getContext("2d") as CanvasRenderingContext2D;
+    
+    this.eventsSubscription = this.signalService.message$.subscribe(() => {  
+      if(this.core_number >=0 ){
+        this.update_chart();
+      }
+      
     });
   }
   ngOnDestroy() {
     this.eventsSubscription.unsubscribe();
   }
 
-  private labels_updated: boolean = false;
-  private on_core_data_updated() {
-    this.lineChartData.labels = [];
-    
-      
-    for (let i = 0; i < this.core_data.length; i++) {
-      this.lineChartData.labels.push('');
+  private update_chart() {
+    this.chart_context.clearRect(0,0,240,100);
+    this.chart_context.beginPath();
+    for(let i = 0; i < this.core_data.length - 1;  i++){
+      this.chart_context?.moveTo(i*2, 100-(this.core_data[i]));
+      this.chart_context?.lineTo((i+1)*2, 100-(this.core_data[i+1]));
     }
-    
-    this.lineChartData.datasets[0].data = this.core_data;
-    //let start = Date.now();
-    this.chart.update('none');
-    //this.chart.render();
-    //let end = Date.now();
+    this.chart_context?.stroke();
 
-    
-    //console.log("Time elapsed ===>", end - start);
   }
+
+
+  /* public clear(){
+    let chart_context = this.chart?.nativeElement.getContext("2d");
+    chart_context?.clearRect(0,0,240,100);
+  } */
 
 }
