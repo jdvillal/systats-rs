3c91a27372d2a2198716b87d9c1af7d63ee6c9d1
Jorge <daniel.--96@hotmail.com>
2023-09-19 18:21:42 -0500

    fix typo in DIskInfo struct

--- src-tauri/src/handlers/disk.rs
+++ src-tauri/src/handlers/disk.rs
@@ -6,10 +6,11 @@ use sysinfo::{DiskExt, System, SystemExt};
 pub struct DiskInfo {
     pub name: String,
     pub disk_type: String,
-    pub moun_point: String,
+    pub mount_point: String,
     pub total_space: u64,
     pub available_space: u64,
     pub removable: bool,
+    pub file_system: String
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -22,7 +23,6 @@ pub fn get_system_disks_information() -> serde_json::Value {
     let mut system_disks_list: Vec<DiskInfo> = Vec::new();
     let mut sys = System::new_all();
     sys.refresh_disks_list();
-    println!("Components: {:?}", sys.components().len());
     let disk_arr = sys.disks();
     for i in 0..disk_arr.len() {
         let type_str = match disk_arr[i].kind() {
@@ -30,13 +30,15 @@ pub fn get_system_disks_information() -> serde_json::Value {
             sysinfo::DiskKind::HDD => "HDD",
             _ => "Unknown",
         };
+        let file_sys: String =  String::from_utf8_lossy(disk_arr[i].file_system()).to_string();
         let disk_info = DiskInfo {
             name: String::from(disk_arr[i].name().to_str().unwrap_or_else(|| "")),
             disk_type: String::from(type_str),
-            moun_point: String::from(disk_arr[i].mount_point().to_str().unwrap_or_else(|| "")),
+            mount_point: String::from(disk_arr[i].mount_point().to_str().unwrap_or_else(|| "")),
             total_space: disk_arr[i].total_space(),
             available_space: disk_arr[i].available_space(),
             removable: disk_arr[i].is_removable(),
+            file_system: file_sys
         };
         system_disks_list.push(disk_info);
     }
