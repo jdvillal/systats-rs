fbe86d7411f86d7c9f8a092d824585a69ba6eda5
Jorge <daniel.--96@hotmail.com>
2023-09-21 18:51:54 -0500

    option to let the user set the app language

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,24 @@ export class AppComponent {
   //title = 'systats-rs';
 
   dark_mode = false;
+  selected_language!: string;
+
+  supported_languages = [
+    {language: 'English', abbreviation: 'en'},
+    {language: 'Español', abbreviation: 'es'}
+  ]
 
   constructor(private translate: TranslateService){
+    let lang = localStorage.getItem('lang');
+    if(lang){
+      this.selected_language = lang;
+      this.set_language(lang);
+      return;
+    }
+    localStorage.setItem('lang', 'en');
     translate.setDefaultLang('en');
     translate.use('en');
+    this.selected_language = 'en';
   }
 
   ngOnInit() { 
@@ -29,12 +43,10 @@ export class AppComponent {
       this.dark_mode = false;
     }
     doc.setAttribute("data-bs-theme", color_mode);
-
   }
 
   onDarkModeToggle(){
     let doc = document.getElementsByTagName("html")[0];
-    
     if(!this.dark_mode){
       doc.setAttribute("data-bs-theme","dark");
       localStorage.setItem("color_mode", "dark");
@@ -46,5 +58,11 @@ export class AppComponent {
     }
   }
 
+  public set_language(language: string){
+    localStorage.setItem('lang', language);
+    this.selected_language = language;
+    this.translate.setDefaultLang(language);
+    this.translate.use(language)
+  }
 
 }
