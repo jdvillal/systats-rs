58276e2a27373a753af954c5db958e5de0652788
Jorge Villalta <daniel.--96@hotmail.com>
2023-09-22 12:06:35 -0500

    macOS title bar style added

--- src/app/title-bar/title-bar.component.ts
+++ src/app/title-bar/title-bar.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { appWindow } from '@tauri-apps/api/window'
 import { OS_type } from '../types/app-types';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-title-bar',
   templateUrl: './title-bar.component.html',
-  styleUrls: ['./title-bar.component.css']
+  styleUrls: ['./title-bar.component.css'],
+  //imports: [CommonModule]
 })
 export class TitleBarComponent {
   @Input() darkmode!: boolean;
@@ -19,7 +21,7 @@ export class TitleBarComponent {
       this.user_OS = 'MacOS';
     } else if (window.navigator.userAgent.indexOf("Linux") != -1) {
       console.log("The user is running Linux");
-      this.user_OS = 'Windows';
+      this.user_OS = 'Linux';
     } else {
       console.log("The user's operating system could not be determined");
     }
@@ -28,7 +30,7 @@ export class TitleBarComponent {
   public toggle_minimize(){
     appWindow.minimize();
   }
-  public toggle_maximiza(){
+  public toggle_maximize(){
     appWindow.toggleMaximize();
   }
   public close(){
