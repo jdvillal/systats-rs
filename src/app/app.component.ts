5268feac313e323e87c9d0568e4562cdbed749fd
Jorge <daniel.--96@hotmail.com>
2023-09-21 21:22:31 -0500

    custom title bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { OS_type } from './types/app-types';
 
 @Component({
   selector: 'app-root',
@@ -17,7 +18,21 @@ export class AppComponent {
     {language: 'Español', abbreviation: 'es'}
   ]
 
+  public user_OS: OS_type = 'Unknown;'
   constructor(private translate: TranslateService){
+    if (window.navigator.userAgent.indexOf("Windows") != -1) {
+      console.log("The user is running Windows");
+      this.user_OS = 'Windows';
+    } else if (window.navigator.userAgent.indexOf("Mac OS") != -1) {
+      console.log("The user is running Mac OS");
+      this.user_OS = 'MacOS';
+    } else if (window.navigator.userAgent.indexOf("Linux") != -1) {
+      console.log("The user is running Linux");
+      this.user_OS = 'Linux';
+    } else {
+      console.log("The user's operating system could not be determined");
+    }
+
     let lang = localStorage.getItem('lang');
     if(lang){
       this.selected_language = lang;
