77e7436142d5ce8429be69463086558f0b8aea36
Jorge Villalta <daniel.--96@hotmail.com>
2023-10-04 21:59:42 -0500

    improve visuals of treemap filetree

--- src/app/page/disks/treemap/treemap.component.ts
+++ src/app/page/disks/treemap/treemap.component.ts
@@ -15,21 +15,21 @@ export class TreemapComponent {
   @Input() path!: string;
   @ViewChild('treemap_chart') chart?: ElementRef<HTMLCanvasElement>;
   private chart_context!: CanvasRenderingContext2D;
-  
+
   public treemap_rectangles: Rectangle[] = [];
-  public canvas_ready  = false;
+  public canvas_ready = false;
   public filetree!: FileTree;
 
   public width = 400.0;
   public height = 400.0;
 
-  ngAfterViewInit(){
+  ngAfterViewInit() {
     if (!this.chart) return;
     this.chart_context = this.chart?.nativeElement.getContext("2d") as CanvasRenderingContext2D;
 
-    if(!this.path) return;
+    if (!this.path) return;
     //invoke tauri command to get a disk filetree and treemap rectangles
-    invoke<any>("get_treemap_from_path", {path: this.path, maxDepth: 2, width: this.width, height: this.height}).then((res)=>{
+    invoke<any>("get_treemap_from_path", { path: this.path, maxDepth: 2, width: this.width, height: this.height }).then((res) => {
       let tree_map_resp = res as TreeMapHandlerResponse;
       this.filetree = tree_map_resp.file_tree;
       this.treemap_rectangles = tree_map_resp.tree_map;
@@ -43,7 +43,7 @@ export class TreemapComponent {
     this.chart_context.beginPath();
     this.chart_context.lineWidth = 1;
     this.chart_context.strokeStyle = "#ff4797"
-   for (let rectangle of this.treemap_rectangles) {
+    for (let rectangle of this.treemap_rectangles) {
       this.chart_context.rect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
     }
     this.chart_context?.stroke();
